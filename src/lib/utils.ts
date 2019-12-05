7bcdf4483e662cf733c81fd663dc499455ace888
Henrik Djärv <henrik@djarv.org>
2019-12-05 17:05:00 +0100

    Add rangegen function to utils
    Add a generator function version of range called rangegen to the utils.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,3 +3,10 @@ export const strToNum = (s: string): number => parseInt(s, 10);
 
 export const range = (count: number, start: number = 1, step: number = 1): number[] =>
   [...Array(count)].map((_, ix) => start + ix * step);
+
+export function* rangegen(count: number, start: number = 1, step: number = 1) {
+  for (let i = 0; i < count; i++) {
+    yield start + i * step;
+  }
+  return null;
+}
