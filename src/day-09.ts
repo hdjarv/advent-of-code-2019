95d33b9e4d477f69c3ee0f8796c39b195aa35104
Henrik Djärv <henrik@djarv.org>
2019-12-15 10:07:32 +0100

    Solution to Day 9

--- src/day-09.ts
+++ src/day-09.ts
@@ -1,3 +1,12 @@
-export default async (...args: any) => {
-  throw Error("Not implemented yet");
+import { run as runProgram } from "./lib/intcode";
+import { readFileIntoNumberArray, join } from "./lib/utils";
+
+export default async (..._args: any) => {
+  const program = readFileIntoNumberArray(join(__dirname, "inputs", "day-09-input.txt")).flat();
+
+  const boostKeyCode = runProgram(program, 1).output.pop();
+  console.log(`Part 1: The BOOST keycode is: ${boostKeyCode}`);
+
+  const coords = runProgram(program, 2).output.pop();
+  console.log(`Part 2: The distress signal coordinates are: ${coords}`);
 };
